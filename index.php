59e840c4f163a83ffcab4d411d9efdca1f578be5
Jasper Helmich <jasper.helmich@gmail.com>
2022-02-09 10:21:31 +0100

    wip

--- index.php
+++ index.php
@@ -2,19 +2,28 @@
 $valet_home_path = getenv('HOME') . '/.config/valet';
 $valet_config = json_decode(file_get_contents("$valet_home_path/config.json"));
 $tld = 'test';
-$parked_path = $valet_config->paths[0];
-$sites = array_filter(scandir($parked_path), function($site) use ($parked_path) {
+$parked_path = $valet_config->paths[1];
+$sites = array_map(function ($site) use ($parked_path) {
+    return [
+        'name' => $site,
+        'modified_at' => filemtime("$parked_path/$site"),
+    ];
+}, array_filter(scandir($parked_path), function($site) use ($parked_path) {
     return is_dir("$parked_path/$site") 
                     && (file_exists("$parked_path/$site/.env") 
                         || file_exists("$parked_path/$site/config.php")
                         || file_exists("$parked_path/$site/public/index.html"));
-});
+}));
+
+
+usort($sites, fn ($a, $b) => $b['modified_at'] - $a['modified_at']);
 ?>
 <html>
     <title>Valet Dashboard</title>
     <head>
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+        <script src="https://mach3insights.test/insight/js/insights.js" insight-key="bjnH0k8N8wNMLSDVeDKg" defer></script>
     </head>
     <body class="font-sans bg-gray-200 m-8">
         <div class="flex items-center justify-center" x-data="app()">
@@ -41,8 +50,8 @@ $sites = array_filter(scandir($parked_path), function($site) use ($parked_path) 
                         <ul class="divide-y divide-gray-300">
                             <template x-for="item in filteredSites" :key="item">
                                 <a class="block p-4 hover:bg-gray-50 cursor-pointer" 
-                                    x-bind:href="'http://'+ item + '.<?=$tld?>/'"
-                                    x-bind:target="'valet_' + item" x-text="item + '.<?=$tld?>'">
+                                    x-bind:href="'http://'+ item.name + '.<?=$tld?>/'"
+                                    x-bind:target="'valet_' + item.name" x-text="item.name + '.<?=$tld?>'">
                                 </a>
                             </template>
                         </ul>
@@ -62,7 +71,7 @@ $sites = array_filter(scandir($parked_path), function($site) use ($parked_path) 
                         }
 
                         return this.siteData.filter((item) => {
-                            return item
+                            return item.name
                                 .toLowerCase()
                                 .includes(this.search.toLowerCase());
                         });
