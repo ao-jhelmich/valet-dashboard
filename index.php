8f62d19fc6a369fef3597f2eeaf93e5afe9edd65
jasper <jasper.helmich@gmail.com>
2024-10-18 11:37:46 +0200

    wip

--- index.php
+++ index.php
@@ -2,19 +2,22 @@
 $valet_home_path = getenv('HOME') . '/.config/valet';
 $valet_config = json_decode(file_get_contents("$valet_home_path/config.json"));
 $tld = 'test';
-$parked_path = $valet_config->paths[1];
-$sites = array_map(function ($site) use ($parked_path) {
-    return [
-        'name' => $site,
-        'modified_at' => filemtime("$parked_path/$site"),
-    ];
-}, array_filter(scandir($parked_path), function($site) use ($parked_path) {
-    return is_dir("$parked_path/$site") 
+$sites = [];
+foreach ($valet_config->paths as $parked_path) {
+    $sites = array_merge($sites, array_map(function ($site) use ($parked_path) {
+        return [
+            'name' => $site,
+            'modified_at' => filemtime("$parked_path/$site"),
+            'is_framework' => file_exists("$parked_path/$site/config.php"),
+            'is_laravel' => file_exists("$parked_path/$site/.env"),
+        ];
+    }, array_filter(scandir($parked_path), function($site) use ($parked_path) {
+        return is_dir("$parked_path/$site") 
                     && (file_exists("$parked_path/$site/.env") 
                         || file_exists("$parked_path/$site/config.php")
                         || file_exists("$parked_path/$site/public/index.html"));
-}));
-
+    })));
+}
 
 usort($sites, fn ($a, $b) => $b['modified_at'] - $a['modified_at']);
 ?>
@@ -23,7 +26,6 @@ usort($sites, fn ($a, $b) => $b['modified_at'] - $a['modified_at']);
     <head>
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
-        <script src="https://mach3insights.test/insight/js/insights.js" insight-key="bjnH0k8N8wNMLSDVeDKg" defer></script>
     </head>
     <body class="font-sans bg-gray-200 m-8">
         <div class="flex items-center justify-center" x-data="app()">
@@ -49,10 +51,18 @@ usort($sites, fn ($a, $b) => $b['modified_at'] - $a['modified_at']);
                     <div class="bg-white shadow-xl rounded-lg w-1/2">
                         <ul class="divide-y divide-gray-300">
                             <template x-for="item in filteredSites" :key="item">
-                                <a class="block p-4 hover:bg-gray-50 cursor-pointer" 
-                                    x-bind:href="'http://'+ item.name + '.<?=$tld?>/'"
-                                    x-bind:target="'valet_' + item.name" x-text="item.name + '.<?=$tld?>'">
-                                </a>
+                                <div 
+                                    x-on:click="window.location.href = 'http://'+ item.name + '.<?=$tld?>/'"
+                                    class="flex p-4 hover:bg-gray-50 cursor-pointer justify-between">
+                                    <a 
+                                        x-ref="valet_{{ item.name }}"
+                                        x-bind:href="'http://'+ item.name + '.<?=$tld?>/'"
+                                        x-text="item.name + '.<?=$tld?>'">
+                                    </a>
+    
+                                    <span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-700/10" x-show="item.is_framework">mach3framework</span>
+                                    <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10" x-show="item.is_laravel">laravel</span>
+                                </div>
                             </template>
                         </ul>
                     </div>
